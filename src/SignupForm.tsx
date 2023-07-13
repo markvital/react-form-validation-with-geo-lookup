901913b19bc69d53d64d942b40767d67fb58ecc9
markvital <mark@fnf.vc>
2023-07-12 21:45:49 -0700

    Minor code cleanup: Extracted form control into separate component; fixed layout jumping on errors

--- src/SignupForm.tsx
+++ src/SignupForm.tsx
@@ -1,137 +1,129 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './SignupForm.css';
 
 interface FormData {
-  firstName: string;
-  lastName: string;
-  state: string;
-  city: string;
-  email: string;
-  password: string;
-}
-
-const SignupForm: React.FC = () => {
-  const [formData, setFormData] = useState<FormData>({
-    firstName: '',
-    lastName: '',
-    state: '',
-    city: '',
-    email: '',
-    password: ''
-  });
-
-  const [errors, setErrors] = useState<Partial<FormData>>({});
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-    setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
-  };
-
-  const validateForm = () => {
-	let isValid = true;
-	const newErrors: Partial<FormData> = {};
+	firstName: string;
+	lastName: string;
+	state: string;
+	city: string;
+	email: string;
+	password: string;
+  }
   
-	// Validate all fields are required
-	Object.keys(formData).forEach((key) => {
-	  const fieldName = key as keyof FormData; // Type assertion
-	  if (formData[fieldName] === '') {
-		newErrors[fieldName] = 'This field is required.';
-		isValid = false;
+  interface FormControlProps {
+	fieldName: keyof FormData;
+	value: string;
+	error: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	autoFocus: boolean;
+  }
+  
+  const FormControl: React.FC<FormControlProps> = ({ fieldName, value, error, onChange, autoFocus }) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+  
+	React.useEffect(() => {
+	  if (autoFocus && inputRef.current) {
+		inputRef.current.focus();
 	  }
+	}, [autoFocus]);
+  
+	const label = fieldName
+	  .replace(/([a-z])([A-Z])/g, '$1 $2') // Split camel case
+	  .split(' ')
+	  .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
+	  .join(' ');
+  
+	return (
+	  <div className="form-group">
+		<input
+		  type="text"
+		  id={fieldName as string}
+		  name={fieldName as string}
+		  value={value}
+		  onChange={onChange}
+		  ref={inputRef}
+		  placeholder={value ? '' : label}
+		/>
+		{error && <span className="error">{error}</span>}
+	  </div>
+	);
+  };
+  
+  const SignupForm: React.FC = () => {
+	const [formData, setFormData] = useState<FormData>({
+	  firstName: '',
+	  lastName: '',
+	  state: '',
+	  city: '',
+	  email: '',
+	  password: ''
 	});
   
-	// Validate email format
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	if (formData.email !== '' && !emailRegex.test(formData.email)) {
-	  newErrors.email = 'Invalid email format.';
-	  isValid = false;
-	}
+	const [errors, setErrors] = useState<Partial<FormData>>({});
   
-	setErrors(newErrors);
-	return isValid;
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	  const { name, value } = e.target;
+	  setFormData((prevData) => ({ ...prevData, [name]: value }));
+	  setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
+	};
+  
+	const validateForm = () => {
+	  let isValid = true;
+	  const newErrors: Partial<FormData> = {};
+  
+	  // Validate all fields are required
+	  Object.keys(formData).forEach((fieldName) => {
+		const fieldValue = formData[fieldName as keyof FormData];
+		if (fieldValue === '') {
+		  newErrors[fieldName as keyof FormData] = 'This field is required.';
+		  isValid = false;
+		}
+	  });
+  
+	  // Validate email format
+	  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	  if (formData.email !== '' && !emailRegex.test(formData.email)) {
+		newErrors.email = 'Invalid email format.';
+		isValid = false;
+	  }
+  
+	  setErrors(newErrors);
+	  return isValid;
+	};
+  
+	const handleSubmit = (e: React.FormEvent) => {
+	  e.preventDefault();
+  
+	  if (validateForm()) {
+		// Perform form submission (e.g., send a POST request)
+		console.log(JSON.stringify(formData));
+	  } else {
+		// Focus on the first input field with an error
+		const firstErrorField = Object.keys(errors)[0];
+		if (firstErrorField) {
+		  const inputRef = document.getElementById(firstErrorField) as HTMLInputElement | null;
+		  if (inputRef) {
+			inputRef.focus();
+		  }
+		}
+	  }
+	};
+  
+	return (
+	  <form className="signup-form" onSubmit={handleSubmit}>
+		{Object.keys(formData).map((fieldName) => (
+		  <FormControl
+			key={fieldName}
+			fieldName={fieldName as keyof FormData}
+			value={formData[fieldName as keyof FormData]}
+			error={errors[fieldName as keyof FormData] || ''}
+			onChange={handleInputChange}
+			autoFocus={fieldName === Object.keys(errors)[0]}
+		  />
+		))}
+		<button type="submit">Submit</button>
+	  </form>
+	);
   };
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (validateForm()) {
-      // Perform form submission (e.g., send a POST request)
-      console.log(JSON.stringify(formData));
-    }
-  };
-
-  return (
-    <form className="signup-form" onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="firstName">First Name</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleInputChange}
-        />
-        {errors.firstName && <span className="error">{errors.firstName}</span>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleInputChange}
-        />
-        {errors.lastName && <span className="error">{errors.lastName}</span>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="state">State</label>
-        <input
-          type="text"
-          id="state"
-          name="state"
-          value={formData.state}
-          onChange={handleInputChange}
-        />
-        {errors.state && <span className="error">{errors.state}</span>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="city">City</label>
-        <input
-          type="text"
-          id="city"
-          name="city"
-          value={formData.city}
-          onChange={handleInputChange}
-        />
-        {errors.city && <span className="error">{errors.city}</span>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
-        {errors.email && <span className="error">{errors.email}</span>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-        />
-        {errors.password && <span className="error">{errors.password}</span>}
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
-
-export default SignupForm;
+  
+  export default SignupForm;
